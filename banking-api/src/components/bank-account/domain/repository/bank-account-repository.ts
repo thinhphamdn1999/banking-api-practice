bd7a8ed6be43e1751c66a79d2ec62ec12bd109ff
Thinh Van Phu Pham <thinh.pham@asnet.com.vn>
2026-02-12 09:41:16 +0700

    feat: create bank account service

---
+++ banking-api/src/components/bank-account/domain/repository/bank-account-repository.ts
@@ -0,0 +1,31 @@
+import { FindOptionsWhere } from 'typeorm';
+
+import { getDataSource } from '@/common/configs/db';
+
+import { ORDER } from '@/common/constants/filters';
+
+import { PaginationOptions } from '@/common/types/pagination';
+import { FilterOptions } from '@/components/bank-account/types/bank-account';
+
+import { BaseRepository } from '@/common/repository/base-repository';
+
+import { BankAccount } from '@/components/bank-account/domain/entities/bank-account.entity';
+
+export class BankAccountRepository extends BaseRepository<BankAccount> {
+  constructor() {
+    super(getDataSource().getRepository(BankAccount));
+  }
+
+  async findBankAccounts(pagination: PaginationOptions, filter?: FilterOptions) {
+    const where: FindOptionsWhere<BankAccount> = {};
+
+    if (filter?.userId) {
+      where.user = { id: filter.userId };
+    }
+
+    return await this.paginate(pagination, {
+      where,
+      order: { createdAt: ORDER.DESC },
+    });
+  }
+}
